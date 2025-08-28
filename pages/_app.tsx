0d655889ffc308d4327c80f28e35ffdd3b59f03f
toshi-studio <contact@toshi.studio>
2025-08-28 15:41:19 +0200

    Deploy updated static export from master

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@radix-ui/themes/styles.css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- utils/assets.ts
+++
@@ -1,5 +0,0 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-export function getAssetPath(path: string): string {
-  return `${basePath}${path}`;
-}
